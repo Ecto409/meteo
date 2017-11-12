9b65736738a09c2762cf4c35be627a2df5fa8397
Votre nom ou pseudoEcto409 <sgoffin1@hotmail.com>
2017-11-12 16:15:51 +0100

    On teste whateverorigin avec https://stackoverflow.com/questions/15005500/loading-cross-domain-endpoint-with-jquery-ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,34 +1,21 @@
 // Exécute un appel AJAX GET
 // Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
+$.ajaxSetup({
+  scriptCharset: "utf-8", //or "ISO-8859-1"
+  contentType: "application/json; charset=utf-8"
+});
 
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
+$.getJSON('http://whateverorigin.org/get?url=' +
+  encodeURIComponent('http://api.wunderground.com') + '&callback=?',
+  function(data) {
+    console.log("> ", data);
 
-  } else if (typeof XDomainRequest != "undefined") {
+    //If the expected response is text/plain
+    $("#viewer").html(data.contents);
 
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
-}
-
-var xhr = createCORSRequest('GET', url);
-if (!xhr) {
-  throw new Error('CORS not supported');
-}
-
+    //If the expected response is JSON
+    //var response = $.parseJSON(data.contents);
+  });
 
 function ajaxGet(url, callback) {
   var req = new XMLHttpRequest();
