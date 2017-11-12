d099321c289e0f64272bb76299b6b20db36e44ca
Votre nom ou pseudoEcto409 <sgoffin1@hotmail.com>
2017-11-12 18:08:11 +0100

    test

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,12 @@
-function getJSON("http://www.whateverorigin.org/get?url=" + encodeURIComponent("http://api.wunderground.com/api/50a65432f17cf542/forecast10day/q/Belgium/Namur.json") + '&callback=?', function(data) {
-  alert(data.contents);
-});
-};
-
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
 function ajaxGet(url, callback) {
-  var req = new XMLHttpRequest();
-  req.open("GET", url);
-  req.addEventListener("load", function() {
-    if (req.status >= 200 && req.status < 400) {
-      // Appelle la fonction callback en lui passant la réponse de la requête
-      callback(req.responseText);
-    } else {
-      console.error(req.status + " " + req.statusText + " " + url);
-    }
-  });
-  req.addEventListener("error", function() {
-    console.error("Erreur réseau avec l'URL " + url);
-  });
-  req.send(null);
+  var xdr = new XMLHttpRequest();
+
+  xdr.onload = function() {
+    alert(xdr.responseText);
+  }
+
+  xdr.open("GET", "http://api.wunderground.com/api/50a65432f17cf542/forecast10day/q/Belgium/Namur.json");
+  xdr.send();
 }
